8dbada06820736babe0938e3749f8c2d79173ee1
zachrickert <zachrickert@gmail.com>
2016-09-02 14:59:33 -0700

    Setup postgres.

--- myproject_sql/scripts/initializedb.py
+++ myproject_sql/scripts/initializedb.py
@@ -42,7 +42,7 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
-
+    settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = get_engine(settings)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
