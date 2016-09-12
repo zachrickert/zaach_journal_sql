9cb314abf5d0c6e954eff66bda90ee77112ce9ab
zachrickert <zachrickert@gmail.com>
2016-09-12 16:23:13 -0700

    Update __init__ to point to right project.

--- myproject_sql/__init__.py
+++ myproject_sql/__init__.py
@@ -10,6 +10,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
-    config.add_static_view(name='static', path='myproject:static')
+    config.add_static_view(name='static', path='myproject_sql:static')
     config.scan()
     return config.make_wsgi_app()
